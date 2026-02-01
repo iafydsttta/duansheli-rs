46389b641a1e0617577952aa813cee8bd31c05f8
Yannik <17107370+iafydsttta@users.noreply.github.com>
2026-02-01 13:14:11 +0100

    wip: delete old files (>24h)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use std::path::Path;
 use std::error::Error;
 use std::time::SystemTime;
+use std::fs::remove_file;
+
 
 pub fn list_dir_with_meta(dir: &str) -> Result<(), Box<dyn Error>> {
+    
     let dir = Path::new(dir);
     if !dir.is_dir() {
         // TODO: useing anyhow crate: return Err(anyhow!("Directory does not exist")); 
@@ -12,8 +15,13 @@ pub fn list_dir_with_meta(dir: &str) -> Result<(), Box<dyn Error>> {
     for entry in dir.read_dir().unwrap() {
         let entry = entry?;
         let metadata = entry.metadata()?;
-        // println!("File: {:?}, Metadata: {:?}, \n", entry.path(), metadata);
-        println!("File: {:?}, Duration since modified: {:?}, \n", entry.path(), SystemTime::now().duration_since(metadata.modified()?).unwrap());
+        let time_since_modified_seconds = SystemTime::now().duration_since(metadata.modified()?).unwrap().as_secs();
+        println!("File: {:?}, Metadata: {:?}, \n", entry.path(), metadata);
+        println!("File: {:?}, Duration since modified: {:?}, \n", entry.path(), time_since_modified_seconds);
+        
+        if time_since_modified_seconds > 24*60*60 {
+            remove_file(&entry.path())?;
+        }
     }
     
     Ok(())
